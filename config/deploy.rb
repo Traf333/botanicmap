8e21c5666dbdb9cfebad1dddd2be98b07365f315
Traf333 <igortr333@gmail.com>
2014-01-16 11:26:03 +0700

    change cap version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,38 +1,21 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+require "bundler/capistrano"
 
-set :application, 'botanicmap'
-set :repo_url, 'git@github.com:Traf333/botanicmap.git'
+server "botanicmap.com", :web, :app, :db, primary: true
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :application, "botanicmap"
+set :user, "plantner"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, "/home/plantner/apps/#{application}"
+set :scm, "git"
+set :repository, "git@github.com:Traf333/#{application}.git"
+set :branch, "master"
 
-# Default value for :scm is :git
-set :scm, :git
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-set :keep_releases, 5
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
   %w[start stop restart].each do |command|
@@ -65,24 +48,4 @@ namespace :deploy do
     end
   end
   before "deploy", "deploy:check_revision"
-
-  # desc 'Restart application'
-  # task :restart do
-  #   on roles(:app), in: :sequence, wait: 5 do
-  #     # Your restart mechanism here, for example:
-  #     # execute :touch, release_path.join('tmp/restart.txt')
-  #   end
-  # end
-
-  # after :publishing, :restart
-
-  # after :restart, :clear_cache do
-  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
-  #     # Here we can do anything such as:
-  #     # within release_path do
-  #     #   execute :rake, 'cache:clear'
-  #     # end
-  #   end
-  # end
-
 end
