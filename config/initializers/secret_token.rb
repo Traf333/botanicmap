15f3bb5a10dde6a1182df7622e348c7b4ba0295f
Traf333 <igortr333@gmail.com>
2014-01-16 10:25:50 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Botanicmap::Application.config.secret_key_base = '592ebb62350f5907cc881ae3a00af2e26f49061ca154ff7240aa80c385b6f179a275beed47cb1524fe72f1dd8417fa3c70898982ee818e94762c5cf124de37c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Botanicmap::Application.config.session_store :cookie_store, key: '_botanicmap_session'
